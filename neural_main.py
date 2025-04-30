6f33a9bd38277ef253a5c733e04a5b8e7d211b0a
DIMITRIOS PSALLIDAS <dimi.psallidas@gmail.com>
2025-04-30 16:47:27 +0300

    νν βοτ 100% ψομπλετε

--- neural_main.py
+++ neural_main.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     print("Training the model...")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = CheckersNet().to(device)
-    trained_model, final_played_game = train(model, device, episodes=1)
+    trained_model, final_played_game = train(model, device, episodes=1000)
     
     # Save final model
     torch.save(trained_model.state_dict(), "checkers_final.pth")
 
-    # workaround for pygame not responding error as model is training
+    # workaround for pygame not responding error as model is training not used
 
     # see last game
     replay_data = final_played_game
